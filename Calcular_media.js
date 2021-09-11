e2c9c15cd16f7783e111180a19adefd3eb3d3eed
Elcilene Aragao <82289849+lene1109@users.noreply.github.com>
2021-09-11 17:21:38 -0300

    Update Calcular_media.js

--- Calcular_media.js
+++ Calcular_media.js
@@ -1,9 +1,4 @@
-/*function calculeMedia(nota1, nota2, nota3) {
-    const soma = (nota1 + nota2 + nota3) 
-    const media = soma/3
-    return media
-}
-console.log(calculeMedia(5,10,8))*/
+
 function MediaAluno(n1, n2){
     
     var media = (n1+n2)/2
